d3f34eab4295cf8968a7db90d23cb6841b51a44b
Muhammad Waqar <mohammad.waqar@xloopdigital.com>
2024-03-11 03:08:24 +0500

    routing issue resolve test

---
+++ app/(other)/layout.tsx
@@ -0,0 +1,17 @@
+
+import "../globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        {children}
+      </body>
+    </html>
+  )
+}
